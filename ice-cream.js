03d6d57e42a370075d838e3406dfa0c76680dcad
nurbek82 <nurbek82@gmail.com>
2020-05-26 16:17:41 -0700

    all forms are validated

--- ice-cream.js
+++ ice-cream.js
@@ -1,4 +1,3 @@
-alert('hi')
 function addError(field) {
   if (field.previousElementSibling &&
     field.previousElementSibling.className === 'error') {
@@ -6,7 +5,7 @@ function addError(field) {
     return;
   }
   const error = document.createElement('div');
-  error.innerHTML = '&#x26A1; '
+  error.innerHTML = '&#x26A1;'
     + field.dataset.errorMsg;
   error.className = 'error';
   field.parentNode.insertBefore(error, field);
@@ -26,7 +25,6 @@ function checkField(field) {
     removeError(field);
   }
 }
-
 function checkSelect(field) {
   if ( field.selectedIndex === 0 ) {
     field.setCustomValidity('Invalid');
@@ -36,24 +34,82 @@ function checkSelect(field) {
     removeError(field);
   }
 }
-
 window.addEventListener('load', function(e) {
-
   const form  = document.getElementById('ice-cream-form');
-/*
-    Add validation for the following fields:
-     * username
-     * email
-     * phone
-     * container
-     * flavor
-     * requests
-     * terms
+  const userName=form.username;
+  userName.dataset.errorMsg='Your input should be between  ' + userName.minLength + ' and ' + userName.maxLength + ' characters '
+  const answer = form.container[0];
+  answer.dataset.errorMsg = "please select one of the options";
+  const userEmail = form.email;
+  userEmail.dataset.errorMsg = 'please type in a valid email';
+  
+  const userPhone = form.phone;
+  userPhone.dataset.errorMsg = 'please type in a valid format as xxx-xxx-xxxx';
 
-    Each field should be validated when its value is changed.
-    All fields should be validated when form is submitted.
-    Be sure to prevent the form from submitting if any field
-     is invalid.
-  */
+  const selectFlavor=form.flavor;
+  selectFlavor.dataset.errorMsg = 'please select a flavor';
 
-});
+  const textArea = form.requests;
+  textArea.dataset.errorMsg = 'your comment should be between' + textArea.minLength + ' and ' + textArea.maxLength + ' characters ';
+  textArea.addEventListener('input', function(e) {
+    checkField(textArea)
+  })
+  const cb = form.terms;
+  cb.dataset.errorMsg = 'You need to check this checkbox';
+  cb.addEventListener('input',function(e){
+    checkField(cb)
+  })
+
+function checkTopping() {
+    const sprinkles = document.getElementById('sprinkles')
+    const nuts = document.getElementById('nuts')
+    const whip = document.getElementById('whip')
+    const toppings = [form.sprinkles,form.nuts,form.whip]
+    for(i=0;i<toppings.length;i++) {
+      toppings[i].addEventListener ('click',function (e) {
+        checkField(sprinkles)
+        checkField(nuts)
+        checkField(whip)
+        })
+      if(toppings[i].checked) {
+        removeError(sprinkles)
+        return;
+        }  
+      toppings[0].dataset.errorMsg = 'please select a topping';
+      addError(sprinkles)
+    }
+  }
+  selectFlavor.addEventListener('change',function (e) {
+    checkSelect(selectFlavor)
+            })
+  for (let button of form.container) {
+    button.addEventListener('click', function(e) {
+      checkField(answer)
+      });
+  }
+  userEmail.addEventListener('input',function(e) {
+    checkField(userEmail)
+  }) 
+  userName.addEventListener('input',function(e) {
+    checkField(userName)
+  }) 
+  userPhone.addEventListener('input',function(e) {
+    checkField(userPhone)
+  });
+  form.addEventListener('submit',function(e){
+    e.preventDefault();
+    checkField(userName);
+    checkField(userEmail);
+    checkField(userPhone);
+    checkField(answer);
+    checkSelect(selectFlavor);
+    checkField(textArea);
+    checkField(cb);
+    checkTopping(sprinkles);
+    
+    if(!form.checkValidity()) {
+      e.preventDefault();
+      alert('please fix form errors')
+       }
+    })
+  })
